62f248d0a4fef294e157855b5ccae18279195f63
Waheed-Labib <labibwahid28@gmail.com>
2025-03-02 14:09:33 +0600

    nodemon installation, and fixed CORS error

--- index.js
+++ index.js
@@ -2,10 +2,34 @@ import express from "express";
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+})
+
 const port = process.env.PORT || 3000;
+
+const fakeData = [
+    {
+        id: 1,
+        content: 'fake data 1'
+    },
+    {
+        id: 2,
+        content: 'fake data 2'
+    },
+    {
+        id: 3,
+        content: 'fake data 3'
+    },
+]
 
 app.get('/', (req, res) => {
     res.send('Translate Quran Server running ...')
+})
+
+app.get('/data', (req, res) => {
+    res.send(fakeData)
 })
 
 app.listen(port, () => {
