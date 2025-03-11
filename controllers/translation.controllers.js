4baac96f4c6c43eb5b51df54c2aae84b2e4399d8
Waheed-Labib <labibwahid28@gmail.com>
2025-03-12 01:56:59 +0600

    added translation controllers and routes

--- controllers/translation.controllers.js
+++ controllers/translation.controllers.js
@@ -1,33 +1,120 @@
-import { ApiError } from "../utils/ApiError";
+import { Translation } from "../models/translation.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
-const addTranslation = () => {
+const getTranslation = asyncHandler(async (req, res) => {
+    const { translatorId, verse_key } = req.query;
+
+    const translation = await Translation.findOne({
+        translator: translatorId,
+        verse_key
+    })
+
+    if (!translation) {
+        throw new ApiError(400, "Translation not found")
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                201,
+                translation,
+                "Get translation successful"
+            )
+        )
+})
+
+const addTranslation = asyncHandler(async (req, res) => {
+
     const { translatorId, verse_key, language, content } = req.body;
     const user = req.user;
 
-    if (user._id !== translatorId) {
-        throw new ApiError(400, "Unauthorized access")
+    if (user._id.toString() !== translatorId) {
+        throw new ApiError(403, "Unauthorized access")
     }
-}
 
-const editTranslation = () => {
+    const alreadyExists = await Translation.findOne({
+        translator: translatorId,
+        verse_key
+    })
+
+    if (alreadyExists) {
+        throw new ApiError(401, "Translation already exists.")
+    }
+
+    const translation = await Translation.create({
+        translator: translatorId,
+        verse_key,
+        language,
+        content
+    })
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                201,
+                translation,
+                'Translation added successfully'
+            )
+        )
+})
+
+const editTranslation = asyncHandler(async (req, res) => {
     const { translatorId, translationId, verse_key, language, content } = req.body;
     const user = req.user;
 
-    if (user._id !== translatorId) {
-        throw new ApiError(400, "Unauthorized access")
+    if (user._id.toString() !== translatorId) {
+        throw new ApiError(403, "Unauthorized access")
     }
-}
 
-const deleteTranslation = () => {
+    const translation = await Translation.findById(translationId);
+
+    if (!translation || translatorId !== translation.translator.toString() || verse_key !== translation.verse_key) {
+        throw new ApiError(400, "Translation not found")
+    }
+
+    translation.language = language;
+    translation.content = content;
+
+    await translation.save();
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                201,
+                translation,
+                "Edit translation successful"
+            )
+        )
+})
+
+const deleteTranslation = asyncHandler(async (req, res) => {
     const { translatorId, translationId } = req.body;
     const user = req.user;
 
-    if (user._id !== translatorId) {
-        throw new ApiError(400, "Unauthorized access")
+    if (user._id.toString() !== translatorId) {
+        throw new ApiError(403, "Unauthorized access")
     }
-}
+
+    await Translation.findByIdAndDelete(translationId)
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                201,
+                {},
+                'Translation deleted successfully'
+            )
+        )
+})
 
 export {
+    getTranslation,
     addTranslation,
     editTranslation,
     deleteTranslation
--- routes/translation.routes.js
+++ routes/translation.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { verifyJWT } from "../middlewares/auth.middleware";
-import { addTranslation, deleteTranslation, editTranslation } from "../controllers/translation.controllers";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { addTranslation, deleteTranslation, editTranslation, getTranslation } from "../controllers/translation.controllers.js";
 
 const router = Router();
 
+router.route('/get-translation').get(getTranslation);
 router.route('/add-translation').post(verifyJWT, addTranslation);
 router.route('/edit-translation').post(verifyJWT, editTranslation);
 router.route('/delete-translation').post(verifyJWT, deleteTranslation);
