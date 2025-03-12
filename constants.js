6d88b931c9c46fcd0b7ef29e6bc237f3cb7b9b4b
Waheed-Labib <labibwahid28@gmail.com>
2025-03-12 16:12:03 +0600

    bug fixed on password length

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 export const DB_NAME = 'Translate_Quran_App_DB';
 
 export const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-export const passwordPattern = /^.{8}$/; // 8 characters long
+export const passwordPattern = /^.{8,}$/; // atleast 8 characters long
 
 export const cookieOptions = {
     httpOnly: true,
